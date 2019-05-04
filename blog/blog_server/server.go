8c1ba378b96bd9cf3dccbba67a6765396e763887
Ben Sooraj <bensoorajm@gmail.com>
2019-05-04 18:20:54 +0530

    blog server setup boilerplate

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
 
+	"github.com/bensooraj/grpc-go-course/blog/blogpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+}
+
+func main() {
+	fmt.Println("Blog Server Started")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v ", err)
+	}
+
+	opts := []grpc.ServerOption{}
+
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
 }
